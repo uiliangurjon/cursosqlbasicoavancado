0b98efc54650b6cdf8ce0ed30cc95203be2bfb01
UILIAN ANDERSON GURJON <uiliangurjon@gmail.com>
2022-03-05 15:24:21 -0300

    Add files via upload

---
+++ insert_regiões.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.tabestadosregioes(nomeregiao)
+	VALUES ('Região Norte'),
+	       ('Região Nordeste'),
+           ('Região Sudeste'),
+           ('Região Sul'),
+           ('Região Centro-Oeste')
